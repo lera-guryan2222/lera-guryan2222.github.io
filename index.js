3788cd54f2eed946657546cd4e530ece1886deee
lera-guryan2222 <167903775+lera-guryan2222@users.noreply.github.com>
2024-05-22 00:07:12 +0400

    Update index.js

--- index.js
+++ index.js
@@ -15,3 +15,17 @@ switchMode.onclick = function(){
     // Сохраняем выбранную тему в localStorage 
     localStorage.setItem('theme', theme.href); 
 }
+document.addEventListener('DOMContentLoaded', () =>{
+    const mediaFiles = document.querySelectorAll('img, video');
+    let i=0
+    Array.from(mediaFiles).forEach((file, index) => {
+        file.onload = () =>{
+            i++
+            percents.innerHTML = ((i*100)/mediaFiles.length).toFixed()
+            if (i===mediaFiles.length){
+                preloader.classList.add('preloader--hide')
+                percents.innerHTML = 100
+            }
+        }
+    })
+})
